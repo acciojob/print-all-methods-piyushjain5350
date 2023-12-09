339d47aabc4ad91182697b4523fc10d1f6a4f7d3
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-12-09 22:23:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	let obj = Math;
+      let ans =  Object.getOwnPropertyNames(obj).filter(function(property) {
+        return typeof obj[property] === 'function';
+      });
+      
+      return ans.join(", ");
 }
 
 alert(allMethods());
